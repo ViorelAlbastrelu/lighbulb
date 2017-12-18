94be203e286be3160a25546190a46edf460cc34f
valbastrelu <viorel.albastrelu@footasylum.com>
2017-12-19 01:12:22 +0200

    added Activity for lvl2

---
+++ app/src/main/java/com/valbastrelu/lightbulb/ActivityLvl2.java
@@ -0,0 +1,13 @@
+package com.valbastrelu.lightbulb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityLvl2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lvl2);
+    }
+}
